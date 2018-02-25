3622996a18f35396384e737d1c4a926b57b3bf21
Glitch (timesta) <none>
2018-02-25 12:30:31 +0000

    👽🐏 Checkpoint views/index.html:634517/61 .gitconfig:634517/27 timestamp.js:634517/81

--- timestamp.js
+++ timestamp.js
@@ -1,24 +1,24 @@
 var moment = require('moment')
  var timestamp = function(time){
     var patt = /^[0-9]*$/g;
-    var isNum = patt.test(time);
+    var isValidNum = patt.test(time);
     // console.log(isNum);
-    if(isNum){
+    if(isValidNum){
         var date = moment.unix(time);
-        var data = {
+        return {
             unix: time,
             natural : date.format('MMMM DD YYYY')
         }
-        return data;
+    
     } else {
         if(moment(time,'MMMM DD YYYY').isValid()){
             var date = moment(time,'MMMM DD YYYY');
-            console.log(time);
-            var data = {
+          
+            return {
                 unix: date.format('X'),
                 natural: time
             }
-            return data;
+           
         } else {
             return {
                 unix: null,
